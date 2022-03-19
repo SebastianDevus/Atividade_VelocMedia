2e46bfbae9d023b624dd7774448256fd0d3df969
SebastianDevus <sebastianjogosextra@gmail.com>
2022-03-19 16:44:43 -0300

    Atividade realizada

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double distancia, tempo, velocidadeMedia;
+
+Console.Write("Insira a distância percorrida, em metros: ");
+distancia = Convert.ToDouble(Console.ReadLine()!);
+
+Console.Write("Insira o tempo gasto: ");
+tempo = Convert.ToDouble(Console.ReadLine()!);
+
+velocidadeMedia = distancia / tempo;
+
+Console.WriteLine($"\nA sua velocidade média foi {velocidadeMedia:N2} m/s");
